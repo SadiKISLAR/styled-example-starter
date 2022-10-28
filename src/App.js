ae3feafe4481dbf1c474bfaaa979ea5aa4accb50
SadiKISLAR <sadi22kislar@gmail.com>
2022-10-28 09:51:02 +0300

    style start

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import Card from "./components/Card";
 import Footer from "./components/Footer";
 import Header from "./components/Header"
 
+const style = {
+  color:{
+    header:"#fff",
+    body:"#eee",
+    footer:"#8A1c4a"
+  },
+  margins:{},
+  responsive:{}
+}
+
 const App = () => {
   return (
     <>
